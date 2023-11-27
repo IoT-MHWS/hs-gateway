ddc939fbad7f333a872497ebd1634b16383a722b
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-11-27 18:42:12 +0300

    move from hs-ensemble

---
+++ src/main/java/artgallery/geteway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package artgallery.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * GatewayApplication
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+
+}
